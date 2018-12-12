bea4e45adf4ba6b79dc9439b9a07bacbd21ab425
litaizi <1103280347@qq.com>
2018-12-12 16:27:40 +0800

    增加柱形图

---
+++ src/components/Bar.vue
@@ -0,0 +1,36 @@
+<template>
+	<div ref="barChart" class="chart">
+  </div>
+</template>
+<script>
+	import echarts from 'echarts/lib/echarts'
+	import 'echarts/lib/chart/bar'
+	export default {
+  name: 'bar',
+  data () { 	
+    return {
+      myChart: '',
+      ass:'aaa'
+    }
+  },
+  props: {
+    barData: {}
+  },
+  mounted () {
+    this.initChart()
+  },
+  methods: {
+    initChart () {
+      this.myChart = echarts.init(this.$refs.barChart)
+      console.log(this.barData)
+      this.myChart.setOption(this.barData)
+    }
+  }
+}
+</script>
+<style scope>
+	.chart{
+    width: 100%;
+    height: 100%;
+  }
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,20 +4,54 @@
     <h2>Essential Links</h2>
     <div style="height: 500px;">
       <LineChart></LineChart>
+      <BarChart :barData="barDataInfo"></BarChart>
     </div>
   </div>
 </template>
 
 <script>
 import LineChart from '@/components/line.vue'
+import BarChart from './bar.vue'
 export default {
   name: 'HelloWorld',
   components: {
-    LineChart
+    LineChart,
+    BarChart
   },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      barDataInfo:{
+        color: ['#3398DB'],
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: { // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [{
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+          axisTick: {
+            alignWithLabel: true
+          }
+        }],
+        yAxis: [{
+          type: 'value'
+        }],
+        series: [{
+          name: '直接访问',
+          type: 'bar',
+          barWidth: '60%',
+          data: [10, 52, 200, 334, 390, 330, 220]
+        }]
+      }
     }
   }
 }
