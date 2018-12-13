ea38b7d9da2f24feefa145fb1a9a977a412254b4
litaizi <1103280347@qq.com>
2018-12-13 15:41:38 +0800

    数据分离

---
+++ src/components/echarts/Scatter.vue
@@ -0,0 +1,35 @@
+<template>
+	<div ref="scatterChart" class="chart">
+  </div>
+</template>
+<script>
+	import echarts from 'echarts/lib/echarts'
+	import 'echarts/lib/chart/scatter'
+	export default {
+  name: 'scatter',
+  data () { 	
+    return {
+      myChart: ''
+    }
+  },
+  props: {
+    scatterData: {}
+  },
+  mounted () {
+    this.initChart()
+  },
+  methods: {
+    initChart () {
+      this.myChart = echarts.init(this.$refs.scatterChart)
+      console.log(this.scatterData)
+      this.myChart.setOption(this.scatterData)
+    }
+  }
+}
+</script>
+<style scope>
+	.chart{
+    width: 100%;
+    height: 100%;
+  }
+</style>
