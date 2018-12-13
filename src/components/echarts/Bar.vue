7a6218accb7e4876249abb361ae5b8f873c65790
litaizi <1103280347@qq.com>
2018-12-13 17:58:18 +0800

    test

--- src/components/echarts/Bar.vue
+++ src/components/echarts/Bar.vue
@@ -5,7 +5,9 @@
 <script>
 	import echarts from 'echarts/lib/echarts'
 	import 'echarts/lib/chart/bar'
+  import chartConfig from './common/echart'
 	export default {
+  mixins: [chartConfig],
   name: 'bar',
   data () { 	
     return {
@@ -16,13 +18,28 @@
     barData: {}
   },
   mounted () {
-    this.initChart()
+    let init = new Promise((resolve) => {
+      this.initChart()
+      resolve()
+    })
+    init.then(() => {
+      this.resizeWindow()
+    })
   },
   methods: {
     initChart () {
       this.myChart = echarts.init(this.$refs.barChart)
-      console.log(this.barData)
-      this.myChart.setOption(this.barData)
+      console.log(this.barData.option)
+      //this.myChart.setOption(this.barData.option)
+      this.myChart.setOption(extend({
+        color: this.colorConfig,
+        title: this.titleConfig,
+        tooltip: this.tooltipConfig,
+        legend: this.legendConfig,
+        xAxis: this.xAxisConfig,
+        yAxis: this.yAxisConfig,
+        series: this.seriesConfig
+      }, this.barData.option))
     }
   }
 }
