c0b7f7f514d6372d3bbc9282de32605c6409b0df
648697377@qq.com <648697377@qq.com>
2018-12-18 17:19:03 +0800

    雷达图

---
+++ src/components/echarts/Radar.vue
@@ -0,0 +1,70 @@
+<template>
+  <div ref="echarts" class="chart">
+  </div>
+</template>
+
+<script>
+import echarts from 'echarts/lib/echarts'
+import 'echarts/lib/chart/radar'
+import chartConfig from './common/echart'
+import { extend } from './common/extend'
+export default {
+  mixins: [chartConfig],
+  data () {
+    return {
+      type: 'radar',
+      radarConfig: {
+        name: {
+          textStyle: {
+            color: '#fff',
+            backgroundColor: '#333',
+            borderRadius: 20,
+            padding: [5, 15]
+          }
+        },
+        indicator: []
+      },
+      seriesConfig: []
+    }
+  },
+  mounted () {
+    let init = new Promise((resolve) => {
+      this.initChart()
+      resolve()
+    })
+    init.then(() => {
+      this.resizeWindow()
+    })
+  },
+  methods: {
+    setData (resolve) {
+      this.legendConfig.data = this.optionData.legendData || []
+      this.radarConfig.indicator = this.optionData.indicator || []
+      this.seriesConfig = this.optionData.seriesData || []
+      for (let item of this.seriesConfig) {
+        item.type = this.type
+      }
+      resolve()
+    },
+    initChart () {
+      new Promise(this.setData).then(() => {
+        this.myChart = echarts.init(this.$refs.echarts)
+        this.myChart.setOption(extend({
+          color: this.colorConfig,
+          title: this.titleConfig,
+          tooltip: this.tooltipConfig,
+          legend: this.legendConfig,
+          radar: this.radarConfig,
+          series: this.seriesConfig
+        }, this.configOption))
+      })
+    }
+  }
+}
+</script>
+<style type="text/css">
+  .chart{
+    width: 100%;
+    height: 100%;
+  }
+</style>
